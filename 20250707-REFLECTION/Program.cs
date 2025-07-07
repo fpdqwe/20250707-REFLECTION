4c2e87f85e920335a29c6b8b79dbe082223c63dd
fpd.core20@gmail.com <fpd.core20@gmail.com>
2025-07-07 21:36:04 +0300

    Добавьте файлы проекта.

---
+++ 20250707-REFLECTION/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _20250707_REFLECTION
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
